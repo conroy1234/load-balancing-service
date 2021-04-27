0060d0007c5bd4ee49097e57ec667fa2b6596f0e
conroy.white <conroy.white@mastek.com>
2021-04-27 16:39:59 +0100

    initial commit

---
+++ src/main/java/com/balancing/service/LoadBalancingServiceApplication.java
@@ -0,0 +1,16 @@
+package com.balancing.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class LoadBalancingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoadBalancingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/balancing/service/LoadBalancingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.balancing.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoadBalancingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
